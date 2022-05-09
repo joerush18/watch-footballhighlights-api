c3fe1cd02d746f590c1372e7ee2236a23c9436f3
joerush18 <joras.aryal23@gmail.com>
2022-05-09 23:21:24 +0545

    small fix for frontend

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const app = express();
 const routes = require("./routes");
+const cors = require("cors");
 
 dotenv.config();
 
+app.use(cors());
 app.use(express.json({ limit: "8mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
